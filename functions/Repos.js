0316856d0152cc21e0c41064e0b1b7c6ab929129
Jorgin <me@jorge.com>
2021-10-23 19:25:01 -0300

    💄 reutilização do css dos repos do usuário em repos curtidos

--- functions/Repos.js
+++ functions/Repos.js
@@ -34,25 +34,6 @@ export async function Repos(user) {
 
         } else break
     }
-
-    if (page > 1) {
-        const $repos = document.querySelectorAll('section.container.result div.repos');
-
-        for (let i = 0; i < $repos.length; i++) {
-            const button_html = `<button class="next ${i + 1 === $repos.length ? 'not-clickabe' : 'clickable'}" data-page="${i + 2}" data-current="${i + 1}" data-action="2">Próximo</button>`
-            if (i === 0) {
-                $repos[i].innerHTML += `<div class="buttons">
-                <button class="prev not-clickabe" data-page="${i}" data-current="${i + 1}" data-action="1">Anterior</button>
-                ${button_html}
-                </div>`
-            } else {
-                $repos[i].innerHTML += `<div class="buttons">
-                <button class="prev clickable" data-page="${i}" data-current="${i + 1}" data-action="1">Anterior</button>
-                ${button_html}
-                </div>`
-            }
-        }
-    }
     
     page--
     ReposCount++
@@ -60,27 +41,5 @@ export async function Repos(user) {
     $header.querySelector('.count').innerText = ReposCount;
     
     $header.classList.add('active');
-    document.querySelector('section.container.result div.repos[data-page="1"]').classList.add('active');
-
-    
-    if (page > 1) {
-        const $buttons = document.querySelectorAll('section.container.result div.repos div.buttons button.clickable')
-        
-        $buttons.forEach($button => {
-            $button.addEventListener('click', () => {
-                const newPage = Number($button.dataset.page);
-                const currentPage = Number($button.dataset.current);
-
-                if (newPage !== 0 && currentPage < pages.length || Number($button.dataset.action) === 1) {
-                    const $result = document.querySelector('section.container.result');
-                    $result.querySelector(`div.repos[data-page="${currentPage}"]`).classList.remove('active');
-                    $result.querySelector(`div.repos[data-page="${newPage}"]`).classList.add('active');
-                    window.scrollTo(0, 0);
-                } else {
-                    console.log(newPage !== 0, currentPage < pages.length, $button.dataset.action === 1);
-                    console.log(newPage, currentPage, $button.dataset.action);
-                }
-            })
-        });
-    }
+    $repos_cont.classList.add('active')
 }
