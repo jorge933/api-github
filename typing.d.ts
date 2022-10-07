8f7482c58197d57aeb73318df5464ed2b5131c87
Jorge Vitor <jorgevitorp9@gmail.com>
2022-10-07 13:35:45 -0300

    🏗️ build(typing.d.ts): declare modules

---
+++ typing.d.ts
@@ -0,0 +1,24 @@
+declare module "*.html" {
+  const value: string;
+  export default value;
+}
+declare module "*.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
